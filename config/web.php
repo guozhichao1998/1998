a9138aa3ba1a6c412644bff4f358ed31d8be269e
xiongahuda <2275106943@qq.com>
2017-09-23 08:08:24 +0800

    1998

---
+++ config/web.php
@@ -0,0 +1,16 @@
+<?php
+// 全局配置
+// return  array(
+
+// 	'db'=>[
+// 		'host'=>'127.0.0.1',
+// 		'username'=>'root',
+// 		'password'=>'123456',
+// 		'dbname'=>'php1505c',
+// 		'tbPrefix'=>'',
+// 		'pConnect'=>0,
+// 	],
+
+
+
+// );
---
+++ controllers/Index.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace controllers;
+use libs\Controller;
+//use models\Joke;
+
+class  Index extends Controller {
+
+	public function index(){
+		echo 1;
+		// $model = new Joke();
+		// $data = $model->select("select * from joke");
+		// //var_dump($data);
+		// $this->assign(['data'=>$data]);
+		// $this->display();
+		// 渲染模板引擎
+	}
+
+}
